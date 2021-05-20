d1a20ce7557067d2ab968b8db7df89470b30570d
Falikor <Bugorv90@mail.ru>
2021-05-20 18:06:04 +0300

    Did t-converter

--- t-converter/ViewController.swift
+++ t-converter/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var cenliusLable: UILabel!
+    
+    @IBOutlet weak var farigetLable: UILabel!
+    
+    @IBOutlet weak var slaider: UISlider! {
+        didSet  {
+            slaider.maximumValue = 100
+            slaider.minimumValue = -100
+            slaider.value = 0
+        }
     }
-
-
+    
+    
+    @IBAction func slaiderChang(_ sender: UISlider) {
+        let temperatureCelsius = Int(round(slaider.value))
+        cenliusLable.text = "\(temperatureCelsius)ºC"
+        let temperatureFariget = 32 + Int(round(slaider.value * 1.8))
+        farigetLable.text = "\(temperatureFariget)ºF"
+    }
+    
+    
+    
 }
 
